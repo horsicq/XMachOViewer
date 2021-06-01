0a5513716b977e9148b527a1abc9069fcb9525b4
horsicq <horsicq@gmail.com>
2021-06-01 19:31:31 +0200

    Fix: 2021-06-01

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cp -f $X_SOURCE_PATH/build/release/xmachoviewer                     /usr/bin/
 cp -f $X_SOURCE_PATH/DEBIAN/xmachoviewer.desktop                    /usr/share/applications/
 cp -Rf $X_SOURCE_PATH/DEBIAN/hicolor/                               /usr/share/icons/
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  /usr/lib/xmachoviewer/
+mkdir -p  /usr/lib/translation/lang
 cp -f $X_SOURCE_PATH/gui_source/translation/*.qm                    /usr/lib/xmachoviewer/lang/
 mkdir -p  /usr/lib/xmachoviewer/signatures
 cp -f $X_SOURCE_PATH/signatures/crypto.db                           /usr/lib/xmachoviewer/signatures/
