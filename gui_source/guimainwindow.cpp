72b0a89af6aee0d6f159a09d8d304f1a19b0bef1
horsicq <horsicq@gmail.com>
2022-11-18 00:07:01 +0100

    Fix: 2022-11-18

--- gui_source/guimainwindow.cpp
+++ gui_source/guimainwindow.cpp
@@ -22,7 +22,8 @@
 
 #include "ui_guimainwindow.h"
 
-GuiMainWindow::GuiMainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::GuiMainWindow)
+GuiMainWindow::GuiMainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::GuiMainWindow)
 {
     ui->setupUi(this);
 
