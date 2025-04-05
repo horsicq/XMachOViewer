3476cf6cc7d9bed6def4cb056afb706577e3b5a7
horsicq <horsicq@gmail.com>
2025-04-06 01:54:22 +0200

    Update file(s): 2025-04-06

--- build/build_macos.sh
+++ build/build_macos.sh
@@ -2,6 +2,8 @@
 cd ..
 mkdir tmp_build
 cd tmp_build
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="~/Qt/5.15.2/clang_64" ..
+cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_PREFIX_PATH="~/Qt/5.15.2/clang_64" ./..
 make
-Pack -G ZIP
+cpack
+cd ..
+cp -Rf tmp_build/packages .
