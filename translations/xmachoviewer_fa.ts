1cb6bb7d07b907024e029dcf01627895ec16ae18
horsicq <horsicq@gmail.com>
2026-08-24 19:09:35 +0200

    feat: add 22 translation files

---
+++ translations/xmachoviewer_fa.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa" sourcelanguage="en_US">
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="../src/gui/dialogabout.ui" line="17"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogabout.ui" line="51"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogabout.cpp" line="36"/>
+        <source>Bugreports</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogabout.cpp" line="40"/>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogabout.cpp" line="45"/>
+        <location filename="../src/gui/dialogabout.cpp" line="50"/>
+        <source>Donate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogabout.cpp" line="55"/>
+        <source>Source code</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogOptions</name>
+    <message>
+        <location filename="../src/gui/dialogoptions.ui" line="17"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogoptions.cpp" line="38"/>
+        <source>Scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogoptions.cpp" line="41"/>
+        <source>Signatures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogoptions.cpp" line="44"/>
+        <source>Hex</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogoptions.cpp" line="47"/>
+        <source>Disasm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogoptions.cpp" line="50"/>
+        <source>Online tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/dialogoptions.cpp" line="53"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GuiMainWindow</name>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="112"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="113"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="114"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="120"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="121"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="122"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="123"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="124"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="125"/>
+        <source>Shortcuts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="126"/>
+        <source>Demangle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="150"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="150"/>
+        <source>All files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="327"/>
+        <location filename="../src/gui/guimainwindow.cpp" line="330"/>
+        <location filename="../src/gui/guimainwindow.cpp" line="333"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="327"/>
+        <source>It is not a valid file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/guimainwindow.cpp" line="330"/>
+        <location filename="../src/gui/guimainwindow.cpp" line="333"/>
+        <source>Cannot open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
